3a31ae31ef5599e0894a33dfbbb204d3d8d1b7a8
CristianCastriota <criscastriota95@gmail.com>
2022-05-01 10:41:12 -0300

    Creación de PaginaDeCompras

---
+++ js/index.js
@@ -0,0 +1,14 @@
+function AgregarAcarrito() {
+    // Clases en JS
+    class ProductoCarrito {
+
+        constructor() { // Obtengo las etiquetas (elementos) del DOM desde JS
+
+            let p = document.getElementById("p");
+            let h5 = document.getElementById("h5");
+            let a = document.getElementById("a");
+        }
+    }
+    console.log(p, h5, a);
+    alert("Se agregó el producto al carrito");
+}
