87d4bef65a4262cdaa6d425b057eb9fdf9aa18bb
CristianCastriota <criscastriota95@gmail.com>
2022-05-04 20:02:55 -0300

    Vista Carrito finalizado

--- js/index.js
+++ js/index.js
@@ -1,20 +1,30 @@
-function AgregarAcarrito() {
+let img;
+let p;
+let h5;
+
+
+function AgregarAcarrito() { // Capturar los datos del formulario con js
     // Clases en JS
-    class ProductoCarrito {
+    //class ProductoCarrito {
 
-        constructor() { // Obtengo las etiquetas (elementos) del DOM desde JS
+    // constructor() { // Obtengo las etiquetas (elementos) del DOM desde JS (index.html)
 
-            let a = document.getElementById("img").value;
-            let p = document.getElementById("p").value;
-            let h5 = document.getElementById("h5").value;
-            //let a = document.getElementById("a").value;
+    let img = document.getElementById("img");
+    let p = document.getElementById("p");
+    let h5 = document.getElementById("h5");
+    //let a = document.getElementById("a").value;
+    localStorage.setItem("imagen", img);
+    localStorage.setItem("precio", h5);
+    localStorage.setItem("descripcion", p);
 
-            localStorage.setItem("imagen", img);
-            localStorage.setItem("precio", h5);
-            localStorage.setItem("descripcion", p);
+    //}
+    //}
 
-        }
-    }
-    console.log(p, h5, a);
     alert("Se agregó el producto al carrito");
+    console.log(p, h5, img);
+
+    console.log(localStorage.getItem("imagen"));
+    console.log(localStorage.getItem("precio"));
+    console.log(localStorage.getItem("descripcion"));
 }
+//localStorage.clear()
