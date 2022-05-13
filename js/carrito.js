74bd9fdbbacba08b3b500181668d28fed1d54dbb
CristianCastriota <criscastriota95@gmail.com>
2022-05-13 16:50:43 -0300

    Carrito Parte 2 finalizado

--- js/carrito.js
+++ js/carrito.js
@@ -1,24 +1,31 @@
 const carrito = JSON.parse(localStorage.getItem('carrito') || []);
 console.log(carrito);
 const listCarrito = document.getElementById('listCarrito');
+const totalpagar = document.getElementById('listTotalPagar');
 
-
+var TotalPagar = 0;
 carrito.forEach(item => {
     const card = `<div class="row">
-                                <img src="${item.img}" class="card-img-top" alt="pava-electrica">
-                                <div class="col-sm-3 mt-4">
+                                <div class="col-sm-3 mt-4 text-center">
+                                <img  src="${item.img}" class="card-img-top" alt="pava-electrica">
+                                </div> 
+                                <div class="col-sm-3 mt-4 text-center">
                                     <h5 style="color: black;">${item.nombre}</h5>
                                     <p>${item.descripcion}</p>
-                                </div>
-                                <div class="col-sm-3">
+                                </div> 
+                                <div class="col-sm-3 mt-4 text-center">
                                     <h5 style="color: black;">$ ${item.precio}</h5>
                                 </div>
-                                <div class="col-sm-3">
+                                <div class="col-sm-3 mt-4 text-center">
                                     <button id="${item.id}" onclick="EliminarCarrito(${item.id})" class="btn btn-danger btn-lg" tabindex="-1" role="button">Eliminar</button>
                                 </div>
                             </div> `;
     listCarrito.innerHTML += card;
+    TotalPagar += item.precio;
+    listTotalPagar.innerHTML = `<h5 style="color: black">$ ${TotalPagar}</h5>`
+
 });
+
 
 const EliminarCarrito = (id) => {
     const listaCarritoModificada = carrito.find(e => e.id === id);
